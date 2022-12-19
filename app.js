47683e7eeebe30ab0ee3b70b7ca554be0e809114
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-12-19 13:38:06 +0200

    'main'

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data() {
+        return {
+          firstName:'Noxcius',
+          lastName:'Pathos',
+          email:'RomeIsGreat@mail.com',
+          state:'Roman',
+          standing:'Emperor',            
+        }
+    },
+})
+
+app.mount('#app')
